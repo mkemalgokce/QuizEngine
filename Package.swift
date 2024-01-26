84d07c3edb29f9f5d18c3b97d8eab38ac88017ce
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-01-26 21:01:13 +0300

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,30 +1,25 @@
-// Package.swift
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "QuizEngine",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "QuizEngine",
-            targets: ["QuizEngine"]
-        ),
+            targets: ["QuizEngine"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "QuizEngine",
-            dependencies: [],
-            path: "QuizEngine"
-        ),
+            path: "QuizEngine"),
         .testTarget(
             name: "QuizEngineTests",
-            dependencies: ["QuizEngine"],
             path: "QuizEngineTests"
-        ),
+            dependencies: ["QuizEngine"]),
     ]
 )
-
